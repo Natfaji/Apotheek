1bd4b5a92378e1cc7b450352b2f5207fcffcd016
Natfaji_A <modemoon2003@hotmail.com>
2022-02-13 22:08:21 +0100

    .

--- index.php
+++ index.php
@@ -38,39 +38,50 @@
 	<main>
 
 		<!-- Slide show -->
-		<div class="slideshow-container">
-
-			<div class="mySlides fade">
-				<div class="numbertext">1 / 3</div>
-				<img src="Assets/Images/1.jpg">
-			</div>
-
-			<div class="mySlides fade">
-				<div class="numbertext">2 / 3</div>
-				<img src="Assets/Images/2.jpg">
-			</div>
-
-			<div class="mySlides fade">
-				<div class="numbertext">3 / 3</div>
-				<img src="Assets/Images/3.jpg">
-			</div>
-
-			<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-			<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-		</div>
-		<br>
-
-		<div class="Dots" style="text-align:center">
-			<span class="dot" onclick="currentSlide(1)"></span>
-			<span class="dot" onclick="currentSlide(2)"></span>
-			<span class="dot" onclick="currentSlide(3)"></span>
-		</div>
-		<!-- Slide show -->
-
 		<section>
-			<div class="services">
-				Onze services
+			<div class="slideshow-container">
+
+				<div class="mySlides fade">
+					<div class="numbertext">1 / 3</div>
+					<img src="Assets/Images/1.jpg">
+				</div>
+
+				<div class="mySlides fade">
+					<div class="numbertext">2 / 3</div>
+					<img src="Assets/Images/2.jpg">
+				</div>
+
+				<div class="mySlides fade">
+					<div class="numbertext">3 / 3</div>
+					<img src="Assets/Images/3.jpg">
+				</div>
+
+				<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+				<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+			</div>
+			<br>
+
+			<div class="Dots" style="text-align:center">
+				<span class="dot" onclick="currentSlide(1)"></span>
+				<span class="dot" onclick="currentSlide(2)"></span>
+				<span class="dot" onclick="currentSlide(3)"></span>
+			</div>
+		</section>
+		<!-- Slide show -->
+		<!-- Nieuws -->
+		<section>
+			<div class="NieuwsTitle">Nieuws</div>
+			<div id="Nieuws">
+				<div class="Nieuws">
+
+				</div>
+				<div class="Nieuws">
+
+				</div>
+				<div class="Nieuws">
+
+				</div>
 			</div>
 		</section>
 
